03cec96da7a351779e519693f4b3eed46244b243
Honghua Zhu <zhh200910@gmail.com>
2023-12-22 16:09:28 +0800

    lealone-fulltext插件初始代码

---
+++ src/test/java/org/lealone/plugins/fulltext/FullTextTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright Lealone Database Group.
+ * Licensed under the Server Side Public License, v 1.
+ * Initial Developer: zhh
+ */
+package org.lealone.plugins.fulltext;
+
+import org.junit.Test;
+import org.lealone.test.TestBase;
+
+public class FullTextTest extends TestBase {
+
+    @Test
+    public void run() {
+    }
+}
